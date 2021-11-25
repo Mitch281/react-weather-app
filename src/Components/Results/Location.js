9705367259580b2bac807cd8249ecfd8ba0af109
Mitchell Anton <antonm@student.unimelb.edu.au>
2021-11-25 13:42:42 +1100

    Added date of today.

--- src/Components/Results/Location.js
+++ src/Components/Results/Location.js
@@ -1,7 +1,27 @@
 const Location = (props) => {
+    
+    // Gets today's date in dd/mm/yyyy format.
+    function getTodaysDate() {
+        const dateToday = new Date();
+        const dayNumber = dateToday.getDate().toString();
+        const monthNumber = (dateToday.getMonth() + 1).toString();
+        const year = dateToday.getFullYear().toString();
+
+        // Adding 0's in front of day and month numbers if necessary to satisfy dd/mm/yyyy format.
+        if (dayNumber.length === 1) {
+            dayNumber = "0" + dayNumber;
+        }
+
+        if (monthNumber.length === 1) {
+            monthNumber = "0" + monthNumber;
+        }
+
+        return dayNumber + "/" + monthNumber + "/" + year;
+    }
+
     return (
-        <h1>The weather today in {props.city}, {props.country}</h1>
-    )
+        <h1>The weather today ({getTodaysDate()}) in {props.city}, {props.country}</h1>
+    );
 }
 
 export default Location
